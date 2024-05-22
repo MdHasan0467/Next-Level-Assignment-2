f34c5d14115156c5041f01a2b2f309858b0582d2
MD Hasan <96623670+MdHasan0467@users.noreply.github.com>
2024-05-22 10:44:38 +0600

    insert data error solved!

--- src/app/modules/product.model.ts
+++ src/app/modules/product.model.ts
@@ -1,43 +1,4 @@
-// import { Schema, model } from 'mongoose';
-// import { Product, Tinventory, Tvariant } from './product/product.interface';
 
-// const VariantSchema = new Schema<Tvariant>({
-//     type: { type: String,required:true},
-//     value:{type: String,required:true}
-// })
-
-// const InventorySchema = new Schema<Tinventory>({
-//     quantity : {type: Number,required:true},
-//     inStock: {type: Boolean,required:true}
-// })
-
-
-// const productSchema = new Schema<Product>({
-//     name: { 
-//         type: String, 
-//     },
-//     description: { 
-//         type: String, 
-//         required: true 
-//     },
-//     price: { 
-//         type: Number, 
-//         required: true 
-//     },
-//     category: { 
-//         type: String, 
-//         required: true 
-//     },
-//     tags: { 
-//         type: [String], 
-//         required: true 
-//     },
-//     variants: VariantSchema,
-//     inventory: InventorySchema
-// })
-
-
-// export const ProductModel = model<Product>('Product', productSchema);
 
 
 
